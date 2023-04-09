caebf28c536da6ac5f7b806bc22bf1b4f425c5f6
sehmuzc <115549234+sehmuzc@users.noreply.github.com>
2023-04-09 09:25:44 +0300

    Initial commit

---
+++ pharmacy/admin.py
@@ -0,0 +1,16 @@
+from .models import Medicine, Prescription, PrescriptionMedicine, ATM, PrescriptionFulfillment, ATMMedicine
+from django.contrib import admin
+
+admin.site.register(Medicine)
+admin.site.register(Prescription)
+admin.site.register(PrescriptionMedicine)
+admin.site.register(ATM)
+admin.site.register(ATMMedicine)
+
+
+class PrescriptionFulfillmentAdmin(admin.ModelAdmin):
+    list_display = ('prescription', 'atm', 'total_price', 'fulfilled_at')
+
+
+admin.site.register(PrescriptionFulfillment, PrescriptionFulfillmentAdmin)
+
---
+++ pharmacy/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from .models import Prescription, Medicine, PrescriptionMedicine, ATM, ATMMedicine
+
+
+class ATMForm(forms.ModelForm):
+    class Meta:
+        model = ATM
+        fields = ['city', 'county', 'total_cash']
+
+class ATMMedicineForm(forms.ModelForm):
+    class Meta:
+        model = ATMMedicine
+        fields = ['medicine', 'stock_level']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['medicine'].queryset = Medicine.objects.all()
+
+
+class PrescriptionForm(forms.ModelForm):
+
+    class Meta:
+        model = Prescription
+        fields = ['patient']
+
+class PrescriptionMedicineForm(forms.ModelForm):
+    class Meta:
+        model = PrescriptionMedicine
+        fields = ['medicine', 'quantity', 'dosage_instructions']
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.price = instance.medicine.price
+        if commit:
+            instance.save()
+        return instance
+
+class SelectATMForm(forms.Form):
+    atm = forms.ModelChoiceField(queryset=ATM.objects.all(), empty_label=None, label='Select an ATM')
+
+
+class AddMedicineToPrescriptionForm(forms.ModelForm):
+    class Meta:
+        model = PrescriptionMedicine
+        fields = ['medicine', 'quantity', 'dosage_instructions']
+
+class AddMedicineToExistingPrescriptionForm(forms.Form):
+    prescription_id = forms.IntegerField()
+    medicine = forms.ModelChoiceField(queryset=Medicine.objects.all())
+    quantity = forms.IntegerField()
+
+    def clean_prescription_id(self):
+        prescription_id = self.cleaned_data.get('prescription_id')
+        try:
+            prescription = Prescription.objects.get(id=prescription_id)
+        except Prescription.DoesNotExist:
+            raise forms.ValidationError("Prescription does not exist.")
+        return prescription
